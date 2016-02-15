53466fbf774befcea00efb42884c06c3582c8c04
Yves Dubromelle <yves+git@dubronetwork.fr>
2016-02-15 17:35:50 +0100

    the script filters everything detected as a dependancy (even a virtual)

--- gentoo-world-cleanup.hs
+++ gentoo-world-cleanup.hs
@@ -3,18 +3,43 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.List as L
 import qualified Data.Text as T
 import           Prelude hiding (FilePath)
 import           Turtle
 
-worldPath :: Text
+-- worldPath = "world"
 worldPath = "/var/lib/portage/world"
 
 main = do
     echo $ "Opening " <> worldPath
     echo ""
+
     fileContent <- strict . input . fromString . T.unpack $ worldPath
     let programsList = T.lines fileContent
-    -- Affichage du contenu complet du fichier
+    echo "* File contents:"
     echo fileContent
+    echo ""
 
+    rawResults <- mapM (\ x -> procStrict "equery" ["--quiet", "depends", x] empty ) programsList
+    let completeResults = zip programsList rawResults
+        (dependsPrograms, endPrograms) = L.partition (\ (_, (x, _)) -> isSuccess x) completeResults
+        newWorldFile = T.unlines . map fst $ endPrograms
+
+    echo "* Raw Results:"
+    print rawResults
+    echo ""
+    echo "* Programs that are not dependencies:"
+    print endPrograms
+    echo ""
+    echo "* Programs that are dependancies of other programs:"
+    print dependsPrograms
+    echo ""
+    echo "* New world file (written to \"world.filtered\")"
+    echo newWorldFile
+    output "world.filtered" (return newWorldFile)
+
+
+isSuccess :: ExitCode -> Bool
+isSuccess ExitSuccess = True
+isSuccess (ExitFailure _) = False
